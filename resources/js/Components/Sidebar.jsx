23851d1c5bc661e2cd6562d080ae14496923cdd6
Affan-97 <affana959@gmail.com>
2023-07-30 20:22:52 +0700

    feat(sidebar): add backgound and change color

--- resources/js/Components/Sidebar.jsx
+++ resources/js/Components/Sidebar.jsx
@@ -6,6 +6,8 @@ import { FaRegCalendarAlt } from "react-icons/fa";
 import { BiLogOut } from "react-icons/bi";
 import { BsListCheck, BsStickyFill, BsPlusLg } from "react-icons/bs";
 import "../../css/Sidebar.css";
+
+import {textColor,listHover} from '../values/Color'
 export default function Sidebar({listprops,tagprops,today,tomorrow,week,all}) {
   console.log(today);
   console.log(tomorrow);
@@ -30,16 +32,37 @@ export default function Sidebar({listprops,tagprops,today,tomorrow,week,all}) {
         "bg-[#4ade80]",
         "bg-[#a1a1aa]",
     ];
+
+
+    const [labelColor, setLabelColor] = useState("text-[#2C3333]");
+    const [labelbg, setLabelbg] = useState("bg-white");
+
     const navRef = useRef();
 
     const compTag = useRef();
     const compList = useRef();
     const btnRef = useRef();
+    const btnTog = useRef();
     const colorTag = useRef();
     const listMenu = useRef();
 
     const listComp = () => {
         compList.current.classList.toggle("hidden");
+        if (labelColor == "text-white") {
+            setLabelColor("text-[#2C3333]")
+            
+        }else{
+            setLabelColor("text-white")
+
+        }
+        if (labelbg == "bg-[#0A81D1]") {
+            setLabelbg("bg-white")
+            
+        }else{
+            setLabelbg("bg-[#0A81D1]")
+
+        }
+        
     };
     const tagComp = () => {
         compTag.current.classList.toggle("hidden");
@@ -85,102 +108,86 @@ export default function Sidebar({listprops,tagprops,today,tomorrow,week,all}) {
     const showNavbar = () => {
         navRef.current.classList.toggle("toggled");
         btnRef.current.classList.toggle("btn-absolute");
+        btnRef.current.classList.toggle("hidden");
+        btnTog.current.classList.toggle("hidden");
+       
     };
     const changeColor = (value, old) => {
         colorTag.current.classList.remove(old);
         setColor(value);
     };
     useEffect(() => {
-        // Update the document title using the browser API
-
         colorTag.current.classList.add(color);
     });
 
     return (
         <>
-            <div
-                ref={navRef}
-                id="sidebar"
-                className="bg-slate-300 p-6 rounded h-full flex flex-col w-[250px] ease-in toggled md:relative  absolute "
+                
+                 <button
+                      ref={btnTog}  
+                        className="font-bold text-xl cursor-pointer fixed -translate-y-2/4 top-2/4 bg-[#000] rounded-r-[50%] px-2 py-4"
+                        onClick={showNavbar}
+                    >
+                        <GiHamburgerMenu className="text-white"/>
+                    </button>
+            
+           <div className="pl-8 py-8 toggled md:relative  absolute ease-in duration-100"
+           ref={navRef}
+           id="sidebar">
+           <div
+                
+                className="bg-white shadow-lg p-6 rounded-3xl h-full flex flex-col w-[250px] "
             >
                 <div className="flex items-center  justify-between mb-4 ">
                     <h1 className="font-bold text-xl">Menu</h1>
                     <button
                         ref={btnRef}
-                        className="font-bold text-xl cursor-pointer btn-absolute "
+                        className="font-bold text-xl cursor-pointer btn-absolute hidden"
                         onClick={showNavbar}
                     >
-                        <GiHamburgerMenu />
+                        <GiHamburgerMenu className={`text-[${textColor}]`} />
                     </button>
                 </div>
-                <form className="flex items-center mb-6">
-                    <label htmlFor="simple-search" className="sr-only">
-                        Search
-                    </label>
-                    <div className="relative w-full">
-                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg
-                                aria-hidden="true"
-                                className="w-5 h-5 text-gray-500"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                    clipRule="evenodd"
-                                />
-                            </svg>
-                        </div>
-                        <input
-                            type="text"
-                            id="simple-search"
-                            className="bg-slate-300 border border-slate-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 "
-                            placeholder="Search"
-                            required
-                        />
-                    </div>
-                </form>
+              
 
                 <div className="mb-6">
-                    <h2 className="font-bold">TASKS</h2>
+                    <h2 className="font-bold text-md">Tasks</h2>
                     <ul>
-                    <li className="w-full p-2  rounded hover:bg-slate-400 flex justify-between">
-                            <Link href={route('home')} className="flex items-center text-md">
+                    <li className={` group w-full p-2  rounded hover:bg-[${listHover}] flex justify-between`}>
+                            <Link href={route('home')} className="w-full flex items-center text-md">
                                 <BsListCheck />
-                                <span className="ml-4 font-semibold hover:font-bold">
+                                <span className="ml-4 font-semibold group-hover:font-bold">
                                     All Task
                                 </span>
                             </Link>
-                            <div className="px-2 rounded bg-slate-100">{all}</div>
+                            <div className={`px-2 rounded bg-slate-200 group-hover:bg-[#78E3FD]`}>{all}</div>
                         </li>
-                        <li className="w-full p-2  rounded hover:bg-slate-400 flex justify-between">
-                            <Link   href={route('upcoming')} className="flex items-center text-md">
+                        <li className={`group w-full p-2 rounded hover:bg-[${listHover}] flex justify-between`}>
+                            <Link   href={route('upcoming')} className="w-full flex items-center text-md">
                                 <MdOutlineDoubleArrow />
-                                <span className="ml-4 font-semibold hover:font-bold">
+                                <span className="ml-4 font-semibold group-hover:font-bold">
                                     Upcoming
                                 </span>
                             </Link>
-                            <div className="px-2 rounded bg-slate-100">{today+tomorrow+week}</div>
+                            <div className={`px-2 rounded bg-slate-200 group-hover:bg-[#78E3FD]`}>{today+tomorrow+week}</div>
                         </li>
-                        <li className="w-full p-2  rounded hover:bg-slate-400 flex justify-between">
-                            <Link  href={route('today')} className="flex items-center text-md">
+                        <li className={`group w-full p-2  rounded hover:bg-[${listHover}] flex justify-between`}>
+                            <Link  href={route('today')} className="w-full flex items-center text-md">
                                 <BsListCheck />
-                                <span className="ml-4 font-semibold hover:font-bold">
+                                <span className="ml-4 font-semibold group-hover:font-bold">
                                     Today
                                 </span>
                             </Link>
-                            <div className="px-2 rounded bg-slate-100">{today}</div>
+                            <div className={`px-2 rounded bg-slate-200 group-hover:bg-[#78E3FD]`}>{today}</div>
                         </li>
-                        <li className="w-full p-2  rounded hover:bg-slate-400 flex justify-between">
-                            <Link href="" className="flex items-center text-md">
+                        <li className={`group w-full p-2  rounded hover:bg-[${listHover}] flex justify-between`}>
+                            <Link href="" className="w-full flex items-center text-md">
                                 <BsStickyFill />
-                                <span className="ml-4 font-semibold hover:font-bold">
+                                <span className="ml-4 font-semibold group-hover:font-bold">
                                     Sticky Wall
                                 </span>
                             </Link>
-                            <div className="px-2 rounded bg-slate-100">1</div>
+                            <div className={`px-2 rounded bg-slate-200 group-hover:bg-[#78E3FD]`}>1</div>
                         </li>
                     </ul>
                 </div>
@@ -191,7 +198,7 @@ export default function Sidebar({listprops,tagprops,today,tomorrow,week,all}) {
                         {listprops.map((index, key) => {
                             //    current.classList.add(index.color)
                             return (
-                                <li className="w-full p-2  rounded hover:bg-slate-400 flex justify-between">
+                                <li className={`w-full p-2  rounded hover:bg-[${listHover}] flex justify-between`}>
                                     <Link
                                         href={route('list',index.slug)}
                                         className="flex items-center text-md"
@@ -210,7 +217,8 @@ export default function Sidebar({listprops,tagprops,today,tomorrow,week,all}) {
                             );
                         })}
                     </ul>
-                    <div className="w-full p-2  rounded hover:bg-slate-400 flex justify-between">
+                   <div className="">
+                     <div className={`w-full p-2  rounded hover:bg-[${listHover}] flex justify-between ${labelbg} ${labelColor} hover:text-[#2C3333]`}>
                         <button
                             onClick={() => listComp()}
                             className="flex items-center text-md w-full"
@@ -223,7 +231,7 @@ export default function Sidebar({listprops,tagprops,today,tomorrow,week,all}) {
                     </div>
                     <div
                         ref={compList}
-                        className="absolute bg-white p-2 rounded hidden -mt-80"
+                        className="absolute left-56 bg-white p-2 rounded hidden ease-in duration-300 shadow-lg"
                     >
                         <div className="flex items-center justify-between mb-4">
                             <div>Add New List</div>
@@ -273,6 +281,7 @@ export default function Sidebar({listprops,tagprops,today,tomorrow,week,all}) {
                             </button>
                         </form>
                     </div>
+                   </div>
                 </div>
                 <hr className="mb-6" />
             
@@ -289,6 +298,7 @@ export default function Sidebar({listprops,tagprops,today,tomorrow,week,all}) {
                     </ul>
                 </div>
             </div>
+           </div>
         </>
     );
 }
---
+++ resources/js/values/Color.js
@@ -0,0 +1,2 @@
+export const  textColor = "#2C3333"
+export const  listHover = "#D1F5FF"
